72120a778daaa513dd2324a8bf89d1d702d87204
Artur C. Fabrício <arturfabricio@gmail.com>
2023-01-06 09:40:41 +0100

    Set up repo with cookiecutter

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='Final project for the DTU course MLOps.',
+    author='Artur C. Fabrício',
+    license='MIT',
+)
