fa2fd6c2aa388f7b460e5a0746696e6967c793da
Cesar-Delafargue <cesar.delafargue@gmail.com>
2023-01-06 11:58:19 +0100

    Created a basic code for training the ResNetModel (not tested yet)

---
+++ src/models/train_model.py
@@ -0,0 +1,48 @@
+import timm 
+import torch
+import torch.nn as nn
+from torch.optim import Adam
+from torch.utils.data import DataLoader
+
+
+
+# Model Hyperparameters
+dataset_path = 'datapath'
+DEVICE = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+batch_size = 100
+epochs = 1
+lr = 0.01
+
+# Training function
+def train ():
+    print("Start training ResNet...")
+
+    model = timm.create_model('resnet18', pretrained=False)
+    model.to(DEVICE)
+
+    train_loader = DataLoader(dataset=train_dataset, batch_size=batch_size, shuffle=True)
+
+    optimizer = Adam(model.parameters(), lr=lr)
+    loss_fn = torch.nn.CrossEntropyLoss()
+
+    model.train()
+    for epoch in range(epochs):
+        print("Epoch 1/{i}...".format(i=epochs))
+        overall_loss = 0
+        for images, labels in trainloader:
+            
+            optimizer.zero_grad()
+            output = model(images)
+            loss = loss_fn(output,labels)
+            loss.backward()
+            optimizer.step()
+
+            overall_loss += loss.item()
+
+        print('Overall loss for this epoch : {i}'.format(i=overall_loss))
+      
+
+train()
+
+
+
