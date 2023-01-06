9f254fa3183452e6300ff5f4781af769e5021157
Cesar-Delafargue <cesar.delafargue@gmail.com>
2023-01-06 13:44:09 +0100

    Some modifications in training function

--- src/models/train_model.py
+++ src/models/train_model.py
@@ -7,17 +7,17 @@ from torch.utils.data import DataLoader
 
 
 # Model Hyperparameters
-dataset_path = 'datapath'
 DEVICE = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 batch_size = 100
 epochs = 1
 lr = 0.01
+chosen_model = 'resnet18'
 
 # Training function
 def train ():
     print("Start training ResNet...")
 
-    model = timm.create_model('resnet18', pretrained=False)
+    model = timm.create_model(chosen_model, pretrained=True)
     model.to(DEVICE)
 
     train_loader = DataLoader(dataset=train_dataset, batch_size=batch_size, shuffle=True)
@@ -25,7 +25,6 @@ def train ():
     optimizer = Adam(model.parameters(), lr=lr)
     loss_fn = torch.nn.CrossEntropyLoss()
 
-    model.train()
     for epoch in range(epochs):
         print("Epoch 1/{i}...".format(i=epochs))
         overall_loss = 0
@@ -39,7 +38,7 @@ def train ():
 
             overall_loss += loss.item()
 
-        print('Overall loss for this epoch : {i}'.format(i=overall_loss))
+        print('Overall loss for this epoch : {i}'.format(i=overall_loss/len(trainloader)))
       
 
 train()
