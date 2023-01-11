b56d8076bf08eb2dbba483669713dbc519bb0c0a
Artur C. Fabrício <arturfabricio@gmail.com>
2023-01-11 22:29:51 +0100

    Data loading almost done

--- src/features/build_features.py
+++ src/features/build_features.py
@@ -3,16 +3,23 @@ import torch
 from torch.utils.data import Dataset, DataLoader
 import pandas as pd
 import os
+import matplotlib.pyplot as plt
 import cv2 as cv2
+from PIL import Image
+from torchvision import transforms
 
 dir_root = Path(__file__).parent.parent.parent
 dataset_raw_images = Path(dir_root, './data/processed/images')
 dataset_raw_labels = Path(dir_root, './data/processed/meta/train.json')
 
-def load_images(dir):
-    x = cv2.imread(str(dir))
-    x = cv2.cvtColor(x, cv2.COLOR_BGR2RGB)
-    return x
+def load_image(path):
+    try:
+        x = cv2.imread(str(path))
+        x = cv2.cvtColor(x, cv2.COLOR_BGR2RGB)  
+        return x
+    except:
+        print('Image doesnt exist')
+        return 0
 
 def prepare_data():
     used_classes = ['apple_pie','baby_back_ribs','beef_tartare', 
@@ -21,18 +28,35 @@ def prepare_data():
                 'pizza','prime_rib']
 
     df = pd.read_json(dataset_raw_labels)
-    df1 = df[used_classes]
-    df_final = pd.DataFrame([])
-    print(df1.head())
+    df_final = df[used_classes]
 
     for _class in used_classes: 
-        df1[_class] = df1.apply(lambda row: str(dataset_raw_images) + "/" + row[_class] + '.jpg', axis=1)
-        df_final.insert(0,'images',df1[_class])
+        df_final[_class] = df_final.apply(lambda row: str(dataset_raw_images) + "/" + row[_class] + '.jpg', axis=1)
 
-    print(df1.head())
+    # df_final = df_final.stack()
+    df_final = df_final.melt(value_name='images')
+    df_final.rename(columns = {'variable':'label'}, inplace = True)
+    
+    class_dict = {'apple_pie':0,
+                  'baby_back_ribs':1,
+                  'beef_tartare':2,  
+                  'caesar_salad':3,
+                  'carrot_cake':4,
+                  'chicken_wings':5,
+                  'donuts':6,          
+                  'french_fries':7,
+                  'grilled_salmon':8,
+                  'lasagna':9,
+                  'omelette':10,        
+                  'pizza':11,
+                  'prime_rib':12}
 
-
-
+    print(df_final.head())
+    df_final['label'] = df_final['label'].apply(lambda x:  class_dict[x])
+    df_final['images'] = df_final['images'].apply(lambda row:  load_image(row))
+    print(df_final.head())
+    df_final.drop(df_final.loc[df_final['images']==0], inplace=True)
+    print(df_final.head())
 
 
     # with np.load(test_path) as data:
@@ -54,10 +78,10 @@ def prepare_data():
             return self.images[idx], self.labels[idx]
 
     # train_dataset = FoodDataset(train_images, train_labels)
-    # train_dataloader = DataLoader(train_dataset, batch_size=64, shuffle=True)
+    # train_dataloader = FoodDataset(train_dataset, batch_size=64, shuffle=True)
 
     # test_dataset = FoodDataset(test_images, test_labels)
-    # test_dataloader = DataLoader(test_dataset, batch_size=64, shuffle=True)
+    # test_dataloader = FoodDataset(test_dataset, batch_size=64, shuffle=True)
 
     # return train_dataloader, test_dataloader
 
