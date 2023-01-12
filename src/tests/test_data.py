58bb6ecc1abe713d94ffc4c57947a40825f03dff
Cesar-Delafargue <cesar.delafargue@gmail.com>
2023-01-12 14:42:10 +0100

    Some unit testing

---
+++ src/tests/test_data.py
@@ -0,0 +1,6 @@
+def test_data():
+    assert 1 == 1
+    
+    
+    
+ 
---
+++ src/tests/test_model.py
@@ -0,0 +1,13 @@
+import torch
+import timm
+
+def test_model():
+
+    train_data = torch.randn(1,3,100,100)
+    model = timm.create_model('resnet18', pretrained=True, num_classes = 101)
+    model.to(torch.device("cuda" if torch.cuda.is_available() else "cpu"))
+
+    output = model(train_data)
+
+    assert output.shape == (1,101)
+
