08275a2f2742e0ec7a4efc618613a06917ea8f8e
Cesar-Delafargue <cesar.delafargue@gmail.com>
2023-01-18 17:05:29 +0100

    Updated unit test for data so it fits new buil_feature func

--- src/tests/test_data.py
+++ src/tests/test_data.py
@@ -10,36 +10,37 @@ sys.path.append(os.path.abspath(data_path))
 
 # Import the data module
 
-from build_features import prepare_data
+from build_features import prepare_data, FoodDataset
 
 @pytest.mark.skipif(
     not os.path.exists('data/'),
-    reason='Data files not found, this should be tested locally',
+    reason='Data files not found, this should be tested locally only',
 )
 def test_data():
 
     num_images = 2000
-    batchsize = 128
+    
+    X_train, X_val, y_train, y_val = prepare_data(num_images)
+    train_dataset = FoodDataset(X_train, y_train)
+    val_dataset = FoodDataset(X_val, y_val)
+
     #assert the number of batch sizes compared to the number of images
-    train_data, val_data = prepare_data(num_images=num_images, batchsize=batchsize)
-    assert (len(train_data) == int(0.8*num_images/batchsize + 1)), "The train batches don't match the amount of data points"
-    assert (len(val_data) == int(0.2*num_images/batchsize + 1)), "The test batches don't match the amount of data points"
+    assert len(train_dataset) == int(num_images*0.8), "The train batches don't match the amount of data points"
+    assert len(val_dataset) == int(num_images*0.2), "The test batches don't match the amount of data points"
 
       
-    #assert that each datapoint has shape [3,224,224]
+    # assert that each datapoint has shape [3,224,224]
 
     bool = True 
-    for images, _ in train_data:
-        for image in images:
-            bool and (image.shape == torch.Size([3, 224, 224]))
+    for image, _ in train_dataset:
+        bool and (image.shape == torch.Size([3, 224, 224]))
     assert bool, "The images don't have the right format" 
 
     #assert that we have the right number of labels compared to the number of imported images
 
     labels_set = set()
-    for _, labels in train_data:
-        for label in labels:
-            labels_set.add(label.item())   
+    for _, label in train_dataset:
+            labels_set.add(label.item()) 
     assert (len(labels_set) == num_images//1000 + 1)
 
  
