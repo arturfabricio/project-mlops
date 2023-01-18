08bae80602a289caf6ce3b59a45cc3c11f3ad8b9
Cesar-Delafargue <cesar.delafargue@gmail.com>
2023-01-18 17:09:31 +0100

    Added a skip marker to training unit test

--- src/tests/test_train.py
+++ src/tests/test_train.py
@@ -6,8 +6,13 @@ import pytest
 data_path = os.path.join(os.path.dirname(__file__), '../models')
 sys.path.append(os.path.abspath(data_path))
 
+
 from train_model import main
 
+@pytest.mark.skipif(
+    not os.path.exists('data/'),
+    reason='Data files not found, this should be tested locally only',
+)
 def test_train():
     assert 1+1 == 2
     
